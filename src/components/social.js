50d6a4fff91f410b1aeb7f781b803145ef637b44
ajiron <ajiron@liveperson.com>
2019-12-18 20:50:00 -0800

    updated icon sizes

--- src/components/social.js
+++ src/components/social.js
@@ -6,20 +6,27 @@ import twitter from '../assets/twitter.svg';
 
 const styles = StyleSheet.create({
   icon: {
+    display: 'inline-block',
+    width: 28,
+    height: 28,
     opacity: 0.7,
-    marginRight: 10,
+    marginRight: 20,
     transition: 'all 250ms ease-in-out',
     cursor: 'pointer',
     ':hover': {
       opacity: 1
     }
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   }
 })
 
 function Icon({ src, url, alt }) {
   return (
     <a href={url} className={css(styles.icon)} target="_blank" rel="noopener noreferrer">
-      <img src={src} alt={alt} />
+      <img src={src} alt={alt} className={css(styles.image)} />
     </a>
   )
 }
